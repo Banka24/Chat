92cad03be3b647abe0ee39252548185c0520525a
Banka24 <Linnod2@yandex.ru>
2024-11-11 18:51:18 +0300

    .

--- Chat.Server/obj/Debug/net8.0/Chat.Server.AssemblyInfo.cs
+++ Chat.Server/obj/Debug/net8.0/Chat.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chat.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b219e3b30db9d395568b71c79c2a591f8380fde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6575afaf699f5e7b628081401970c43605d28403")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chat.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chat.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
