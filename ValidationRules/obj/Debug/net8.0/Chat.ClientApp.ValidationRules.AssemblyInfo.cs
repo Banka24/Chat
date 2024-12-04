9a7677acfae142e2f79245ea5e8fb95f7f13c8e2
Banka24 <Linnod2@yandex.ru>
2024-12-04 15:42:59 +0300

    update

--- ValidationRules/obj/Debug/net8.0/Chat.ClientApp.ValidationRules.AssemblyInfo.cs
+++ ValidationRules/obj/Debug/net8.0/Chat.ClientApp.ValidationRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chat.ClientApp.ValidationRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f1e236a11ae35d0858c949c32b288b726498bb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a36dd08c52cecdc33962983aed1d644d04aafce5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chat.ClientApp.ValidationRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chat.ClientApp.ValidationRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
