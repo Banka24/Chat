044d2aa17f857df94aa07bfae38fbfeab2ea5e43
Banka24 <Linnod2@yandex.ru>
2024-12-17 16:45:54 +0300

    .

--- Chat.ClientApp.Models/obj/Debug/net8.0/Chat.ClientApp.Models.AssemblyInfo.cs
+++ Chat.ClientApp.Models/obj/Debug/net8.0/Chat.ClientApp.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chat.ClientApp.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b610931b917eef2ee6d1d12ee39861997ca67ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbab6ed7645b58ba87a8588ebe0a922bf49b3fa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chat.ClientApp.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chat.ClientApp.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
