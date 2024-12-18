8c9ae2289c16714cfc04a23eae9eb8de7ff3f0f6
Banka24 <Linnod2@yandex.ru>
2024-12-18 20:24:32 +0300

    update

--- Chat.ClientApp.Services/obj/Debug/net8.0/Chat.ClientApp.Services.AssemblyInfo.cs
+++ Chat.ClientApp.Services/obj/Debug/net8.0/Chat.ClientApp.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chat.ClientApp.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+044d2aa17f857df94aa07bfae38fbfeab2ea5e43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+507fbe532ff51df7a3f894ea6171d5424382de26")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chat.ClientApp.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chat.ClientApp.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
